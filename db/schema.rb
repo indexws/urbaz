889bea1cba61de02d3374adef11e6a661bea8983
Silvio Geoffroy <sageoffroy@gmail.com>
2020-12-16 16:54:06 -0300

    Template scaffold edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_231959) do
-
-  create_table "pepes", force: :cascade do |t|
-    t.text "atributo"
-    t.integer "atributro2"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_11_16_191852) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
